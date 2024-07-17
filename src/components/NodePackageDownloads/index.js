3a3a9c43dc42dbe6066938879137d1a525436ce0
chanderprakash20 <chander132002@gmail.com>
2024-07-17 17:37:17 +0530

    add Show more versions Button

--- src/components/NodePackageDownloads/index.js
+++ src/components/NodePackageDownloads/index.js
@@ -1,52 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import StableRelease from './StableRelease';
-import CurrentRelease from './CurrentRelease';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
-import './index.css';
+import React, { useEffect, useState } from 'react'
+import StableRelease from './StableRelease'
+import CurrentRelease from './CurrentRelease'
+import Tabs from '@theme/Tabs'
+import TabItem from '@theme/TabItem'
+import './index.css'
 
 export default function NodePackageDownloads() {
-  const [releases, setReleases] = useState([]);
-  const [currentRelease, setCurrentRelease] = useState();
-  const [start, setStart] = useState(0);
-  const [showPaginationButton, setShowPaginationButton] = useState(true);
-  const [showStableReleases, setShowStableReleases] = useState(false); // New state variable
-
-  const size = 10;
+  const [releases, setReleases] = useState([])
+  const [currentRelease, setCurrentRelease] = useState()
+  const size = 10
+  const [start, setStart] = useState(0)
+  const [showPaginationButton, setShowPaginationButton] = useState(true)
+  const [showVersions, setShowVersions] = useState(false)
+  const [allVersionsLoaded, setAllVersionsLoaded] = useState(false)
 
   useEffect(() => {
-    fetchReleases();
-  }, []);
+    fetchReleases()
+  }, [])
 
   const fetchReleases = () => {
-    fetch(
-      'https://airdrop-api.klaytn.foundation/node/releases?start=' + start,
-      {
-        method: 'GET',
-      }
-    )
+    fetch('https://airdrop-api.klaytn.foundation/node/releases?start=' + start, {
+      method: 'GET',
+    })
       .then((response) => response.json())
       .then((response) => {
-        let releasesData = response.data.releases;
-        let machineTypes = response.data.machineTypes.filter(item => item.machineType !== "windows");
-        let config = response.data.config;
-        setReleases([...releases, ...releasesData]);
+        let releasesData = response.data.releases
+        let machineTypes = response.data.machineTypes.filter(item => item.machineType !== "windows")
+        let config = response.data.config
+        setReleases([...releases, ...releasesData])
 
-        let resultFirstRecord;
+        let resultFirstRecord
         if (releasesData && releasesData.length > 0 && start === 0) {
           resultFirstRecord = {
             tagName: releasesData[0].tag_name,
             binaryPrefix: releasesData[0].type,
             githubUrl: config.gitBaseUrls[releasesData[0].type],
             machineTypes: machineTypes,
-          };
+          }
 
-          setCurrentRelease(resultFirstRecord);
+          setCurrentRelease(resultFirstRecord)
         }
-        setStart(start + size);
-        setShowPaginationButton(releasesData.length !== 0);
-      });
-  };
+        setStart(start + size)
+        setShowPaginationButton(releasesData.length !== 0)
+        if (releasesData.length < size) {
+          setAllVersionsLoaded(true)
+        }
+      })
+  }
+
+  const handleShowVersionsClick = () => {
+    setShowVersions(true)
+    if (!allVersionsLoaded) {
+      fetchReleases()
+    }
+  }
 
   return (
     <div style={{ marginBottom: '20px' }}>
@@ -79,7 +86,7 @@ export default function NodePackageDownloads() {
               >
                 <CurrentRelease
                   tabConfig={currentRelease.machineTypes.filter(
-                    (_machine) => _machine.machineType === _tab.machineType
+                    (_machine) => _machine.machineType == _tab.machineType
                   )}
                   releaseData={{
                     tagName: currentRelease.tagName,
@@ -91,83 +98,72 @@ export default function NodePackageDownloads() {
             ))}
           </Tabs>
         ) : (
-          <div style={{ width: '100%' }}>
-            <div style={{ margin: '10px auto', width: '100px' }}>
-              <span className="loader"></span>
+          <>
+            <div style={{ width: '100%' }}>
+              <div style={{ margin: '10px auto', width: '100px' }}>
+                <span className="loader"></span>
+              </div>
             </div>
-          </div>
+          </>
         )}
       </div>
-
       <div style={{ border: '0.5px solid', marginTop: '30px' }}>
-        <button
-          onClick={() => setShowStableReleases(!showStableReleases)}
-          className="show-versions-button" // Apply the new class
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            border: '0.5px solid',
+          }}
         >
-          {showStableReleases ? 'Hide Versions' : 'Show Versions'}
-        </button>
-        {showStableReleases && (
-          <div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                border: '0.5px solid',
-              }}
-            >
-              <div
-                style={{
-                  fontSize: '20px',
-                  fontWeight: 600,
-                  padding: '10px',
-                  width: '200px',
-                  borderRight: '0.5px solid',
-                  alignContent: 'center',
-                }}
+          <div
+            style={{
+              fontSize: '20px',
+              fontWeight: 600,
+              padding: '10px',
+              width: '200px',
+              borderRight: '0.5px solid',
+              alignContent: 'center',
+            }}
+          >
+            Archived releases
+          </div>
+          <div style={{ alignContent: 'center', padding: '10px' }}>
+            These are the current and previous releases of Kaia, updated
+            automatically when a new version is tagged in our{' '}
+            <a style={{ textDecoration: 'underline' }} href={currentRelease?.githubUrl}>
+              GitHub repository.
+            </a>
+          </div>
+        </div>
+        {showVersions && (
+          <Tabs groupId="machineTypes">
+            {currentRelease.machineTypes.map((_tab, _index) => (
+              <TabItem
+                key={_index}
+                value={_tab.machineType.toLocaleLowerCase()}
+                label={_tab.machineType.toUpperCase()}
+                default={_tab.default}
               >
-                Archived Releases 
-              </div>
-              <div style={{ alignContent: 'center', padding: '10px' }}>
-                These are the current and previous releases of Kaia, updated
-                automatically when a new version is tagged in our{' '}
-                <a
-                  style={{ textDecoration: 'underline' }}
-                  href={currentRelease?.githubUrl}
-                >
-                  GitHub repository.
-                </a>
-              </div>
-            </div>
-            {currentRelease && currentRelease.machineTypes ? (
-              <Tabs groupId="machineTypes">
-                {currentRelease.machineTypes.map((_tab, _index) => (
-                  <TabItem
-                    key={_index}
-                    value={_tab.machineType.toLocaleLowerCase()}
-                    label={_tab.machineType.toUpperCase()}
-                    default={_tab.default}
-                  >
-                    <StableRelease
-                      tabConfig={currentRelease.machineTypes.filter(
-                        (_machine) => _machine.machineType === _tab.machineType
-                      )}
-                      releaseData={releases}
-                      fetchReleases={fetchReleases}
-                      showPaginationButton={showPaginationButton}
-                    />
-                  </TabItem>
-                ))}
-              </Tabs>
-            ) : (
-              <div style={{ width: '100%' }}>
-                <div style={{ margin: '10px auto', width: '100px' }}>
-                  <span className="loader"></span>
-                </div>
-              </div>
-            )}
+                <StableRelease
+                  tabConfig={currentRelease.machineTypes.filter(
+                    (_machine) => _machine.machineType == _tab.machineType
+                  )}
+                  releaseData={releases}
+                  fetchReleases={fetchReleases}
+                  showPaginationButton={showPaginationButton}
+                />
+              </TabItem>
+            ))}
+          </Tabs>
+        )}
+        {!showVersions && (
+          <div style={{ textAlign: 'center', margin: '20px' }}>
+            <button onClick={handleShowVersionsClick} className="show-versions-button">
+              Show more versions
+            </button>
           </div>
         )}
       </div>
     </div>
-  );
+  )
 }
