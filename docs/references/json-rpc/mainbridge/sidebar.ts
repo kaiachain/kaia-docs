d7b9a7c24a4eedf06b7cfe95e48780ff696936c9
Sotatek-TanHoang <tan.hoang@sotatek.com>
2024-07-19 15:44:08 +0700

    feat: keep json-rpc

--- docs/references/json-rpc/mainbridge/sidebar.ts
+++ docs/references/json-rpc/mainbridge/sidebar.ts
@@ -6,42 +6,6 @@ const sidebar: SidebarsConfig = {
       type: "doc",
       id: "references/json-rpc/mainbridge/web-3-rpc",
     },
-    {
-      type: "category",
-      label: "mainbridge",
-      items: [
-        {
-          type: "doc",
-          id: "references/json-rpc/mainbridge/convert-child-chain-block-hash-to-parent-chain-tx-hash",
-          label: "mainbridge_convertChildChainBlockHashToParentChainTxHash",
-          className: "api-method post",
-        },
-        {
-          type: "doc",
-          id: "references/json-rpc/mainbridge/get-child-chain-indexing-enabled",
-          label: "mainbridge_getChildChainIndexingEnabled",
-          className: "api-method post",
-        },
-        {
-          type: "doc",
-          id: "references/json-rpc/mainbridge/node-info",
-          label: "mainbridge_nodeInfo",
-          className: "api-method post",
-        },
-        {
-          type: "doc",
-          id: "references/json-rpc/mainbridge/add-peer",
-          label: "mainbridge_addPeer",
-          className: "api-method post",
-        },
-        {
-          type: "doc",
-          id: "references/json-rpc/mainbridge/remove-peer",
-          label: "mainbridge_removePeer",
-          className: "api-method post",
-        },
-      ],
-    },
   ],
 };
 
