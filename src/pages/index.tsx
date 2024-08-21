962f848efe23cd8a5c012339409a001de898be2a
scott lee <scott.l@klaytn.foundation>
2024-08-21 12:14:05 +0900

    Fix flash of unstyled content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import styled from 'styled-components';
@@ -38,16 +38,28 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext()
+  const { siteConfig } = useDocusaurusContext();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Welcome to the Kaia Docs"
     >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <div className={`layout-wrapper ${isLoaded ? 'loaded' : ''}`}>
+        <HomepageHeader />
+        <main>
+          <HomepageFeatures />
+        </main>
+      </div>
     </Layout>
-  )
+  );
 }
