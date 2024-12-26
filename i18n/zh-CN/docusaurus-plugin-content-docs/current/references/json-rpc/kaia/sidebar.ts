e7aad1cb59a3fa7e853f607a5fae4b70a5160c2f
kaia-docs <content+kaia@klaytn.foundation>
2024-12-27 01:29:41 +0800

    New translations sidebar.ts (Chinese Simplified)

--- i18n/zh-CN/docusaurus-plugin-content-docs/current/references/json-rpc/kaia/sidebar.ts
+++ i18n/zh-CN/docusaurus-plugin-content-docs/current/references/json-rpc/kaia/sidebar.ts
@@ -2,7 +2,7 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 const sidebar: SidebarsConfig = {
   apisidebar: [{
     type: "doc",
-    id: "references/json-rpc/kaia/web-3-rpc"
+    id: "references/json-rpc/klay/web-3-rpc"
   }]
 };
 export default sidebar.apisidebar;
