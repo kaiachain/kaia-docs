1dff3f3dcb0729269e54d10f43f0cb73e62425ff
kaia-docs <content+kaia@klaytn.foundation>
2025-02-04 20:28:53 +0800

    New translations sidebar.ts (Japanese)

--- i18n/ja/docusaurus-plugin-content-docs/current/references/json-rpc/governance/sidebar.ts
+++ i18n/ja/docusaurus-plugin-content-docs/current/references/json-rpc/governance/sidebar.ts
@@ -1,7 +1,7 @@
 import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 const sidebar: SidebarsConfig = {
   apisidebar: [{
-    type: "doc",
+    type: "ドキュメント",
     id: "references/json-rpc/governance/web-3-rpc"
   }]
 };
