dbd69cebd81614025fee4c01930f4479c0154e3e
kaia-docs <content+kaia@klaytn.foundation>
2025-02-10 17:07:45 +0800

    New translations sidebar.ts (Japanese)

--- i18n/ja/docusaurus-plugin-content-docs/current/references/json-rpc/net/sidebar.ts
+++ i18n/ja/docusaurus-plugin-content-docs/current/references/json-rpc/net/sidebar.ts
@@ -2,7 +2,7 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 const sidebar: SidebarsConfig = {
   apisidebar: [{
     type: "doc",
-    id: "references/json-rpc/net/web-3-rpc"
+    id: "references/json-rpc/eth/web-3-rpc"
   }]
 };
 export default sidebar.apisidebar;
