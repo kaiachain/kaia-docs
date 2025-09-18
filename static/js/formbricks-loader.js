b2574fbf35d52b7124708a94bbae9dc775c471d2
scott lee <scott.l@kaia.io>
2025-09-18 16:09:56 +0900

    Update env id for survey system

--- static/js/formbricks-loader.js
+++ static/js/formbricks-loader.js
@@ -6,7 +6,7 @@
   t.onload=function(){
     window.formbricks
       ? window.formbricks.setup({
-          environmentId:"cm99ktuyy0004my01fv4i7nad",
+          environmentId:"cm99ktuzi0009my015zzcv3j1",
           appUrl:"https://formbricks.lkw1615.synology.me"
         })
       : console.error("Formbricks library failed to load properly.");
